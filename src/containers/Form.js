a597fe5e2d994078ab2f615866e35146dc25432a
reardek <jjkowalinski@yahoo.com>
2021-02-23 11:14:20 +0100

    Fix syntax bug

--- src/containers/Form.js
+++ src/containers/Form.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import styled from 'styled-components';
-import ItemsContext from '../Context/ItemsContextProvider';
-import SubHeader from '../components/Header/SubHeader';
-import FormItem from '../components/FormItem/FormItem';
-import Button from '../components/Button/Button';
+import React from "react";
+import styled from "styled-components";
+import { ItemsContext } from "../Context/ItemsContextProvider";
+import SubHeader from "../components/Header/SubHeader";
+import FormItem from "../components/FormItem/FormItem";
+import Button from "../components/Button/Button";
 
 const FormWrapper = styled.div`
   display: flex;
@@ -19,43 +19,56 @@ const SubmitButton = styled(Button)`
 
 const Form = ({ match, history }) => {
   const { addItemRequest } = React.useContext(ItemsContext);
-  const [title, setTitle] = React.useState('');
-  const [quantity, setQuantity] = React.useState('');
-  const [price, setPrice] = React.useState('');
+  const [title, setTitle] = React.useState("");
+  const [quantity, setQuantity] = React.useState("");
+  const [price, setPrice] = React.useState("");
 
-  const handleOnSubmit = e => {
+  const handleOnSubmit = (e) => {
     e.preventDefault();
     addItemRequest({
       title,
       quantity,
       price,
       id: Math.floor(Math.random() * 100),
-      listId: parseInt(match.params.id)
+      listId: parseInt(match.params.id),
     });
     history.goBack();
-  }
-  
-  return(
-  <>
-    {history && (
-      <SubHeader goBack={() => history.goBack()} title={`Add Item`} />
-    )}
-    <FormWrapper>
-      <form onSubmit={handleOnSubmit}>
-        <FormItem id='title' label='Title' placeholder='Insert title' value={title} handleOnChange={setTitle} />
-        <FormItem
-          id='quantity'
-          label='Quantity'
-          type='number'
-          placeholder='0'
-          value={quantity}
-          handleOnChange={setQuantity}
-        />
-        <FormItem id='price' label='Price' type='number' placeholder='0.00' value={price} handleOnChange={setPrice} />
-        <SubmitButton>Add Item</SubmitButton>
-      </form>
-    </FormWrapper>
-  </>
+  };
+
+  return (
+    <>
+      {history && (
+        <SubHeader goBack={() => history.goBack()} title={`Add Item`} />
+      )}
+      <FormWrapper>
+        <form onSubmit={handleOnSubmit}>
+          <FormItem
+            id="title"
+            label="Title"
+            placeholder="Insert title"
+            value={title}
+            handleOnChange={setTitle}
+          />
+          <FormItem
+            id="quantity"
+            label="Quantity"
+            type="number"
+            placeholder="0"
+            value={quantity}
+            handleOnChange={setQuantity}
+          />
+          <FormItem
+            id="price"
+            label="Price"
+            type="number"
+            placeholder="0.00"
+            value={price}
+            handleOnChange={setPrice}
+          />
+          <SubmitButton>Add Item</SubmitButton>
+        </form>
+      </FormWrapper>
+    </>
   );
 };
 
